dca1af44a19713b18e3b31c92485dd29dfbddafd
NicollahSeketeIQ <nicollah.sekete@uniqueiq.co.uk>
2023-08-31 16:58:25 +0100

    first commit to master

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css'
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="db2cbcbf-c56f-4655-85ca-d748f47d2f23
+            "
+            userName="FirstUser"
+            userSecret="password"
+        />
+    )
+}
+
+export default App;
