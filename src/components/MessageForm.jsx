be096fb81ec66657ab68a43e1f90dd18d8f98aba
NicollahSeketeIQ <nicollah.sekete@uniqueiq.co.uk>
2023-09-06 15:54:32 +0100

    added to chat feed, image and read receipts

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { sendMessage, isTyping } from "react-chat-engine";
+import { sendOutlined, PictureOutlined, SendOutlined } from "@ant-design/icons";
 
 const MessageForm = (props) => {
   const [value, setValue] = useState("");
@@ -8,13 +9,17 @@ const MessageForm = (props) => {
     event.preventDefault();
     const text = value.trim();
 
-    if(text.length > 0) sendMessage(creds, chatId, {text})
-
+    if (text.length > 0) sendMessage(creds, chatId, { text });
+    setValue("");
   };
 
   const handleChange = (event) => {
     setValue(event.target.value);
     isTyping(props, chatId);
+  };
+
+  const handleUpload = (event) => {
+    sendMessage(creds, chatId, { files: event.target.files, text: "" });
   };
 
   return (
@@ -26,6 +31,21 @@ const MessageForm = (props) => {
         onChange={handleChange}
         onSubmit={handleSubmit}
       />
+      <label htmlFor="upload-button">
+        <span className="image-button">
+          <PictureOutlined className="picture-icon" />
+        </span>
+      </label>
+      <input
+        type="file"
+        multiple={false}
+        id="upload-button"
+        style={{ display: "none" }}
+        onChange={handleUpload}
+      />
+      <button type="submit" className="send-button">
+        <SendOutlined className="send-icon"/>
+      </button>
     </form>
   );
 };
