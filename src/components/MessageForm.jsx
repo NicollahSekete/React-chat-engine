8a90f1e1534c696ed8eea6d911b6ade3b16a0276
NicollahSeketeIQ <nicollah.sekete@uniqueiq.co.uk>
2023-09-06 15:18:39 +0100

    message Form functions

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,5 +1,33 @@
-const MessageForm = ()=>{
-    return <div>MessageForm</div>;
-}
+import { useState } from "react";
+import { sendMessage, isTyping } from "react-chat-engine";
 
-export default MessageForm
+const MessageForm = (props) => {
+  const [value, setValue] = useState("");
+  const { chatId, creds } = props;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const text = value.trim();
+
+    if(text.length > 0) sendMessage(creds, chatId, {text})
+
+  };
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+    isTyping(props, chatId);
+  };
+
+  return (
+    <form className="message-form" onSubmit={handleSubmit}>
+      <input
+        className="message-input"
+        placeholder="send a message..."
+        value={value}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+    </form>
+  );
+};
+
+export default MessageForm;
